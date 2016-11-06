cafb9c646787f12896df29c52cab6b9c9a4433b0
Federico Vitale <fedevitale99@gmail.com>
2016-11-06 13:55:10 +0100

    Bug fix
    Previously javascript scale doesn't works well.

--- dist/js/scale.js
+++ dist/js/scale.js
@@ -1,74 +1,100 @@
-(function($) {
-  $.fn.scale = function(c, opt) {
-    var me, s, settings;
+(function( $ ) {
+  $.fn.scale = function ( factor, options ) {
+    "use strict";
+
+    // Declarations
+    var me, settings, l, size;
+
+    // Conditions
+    if ( !factor ) {
+      factor = 1;
+    } else if ( factor == 'random' ) {
+      function randomInt(min,max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+      }
+      factor = randomInt(1, 10)
+    }
+
+    // Setting `me` as `this`
     me = this;
+
+    // Options, case if unit is not defined use pixels.
     settings = $.extend({
-      format: 'px'
-    }, opt);
-    if (me !== "undefined") {
-      s = me.css('font-size').slice(0, me.length - 2);
-      me.css('font-size', s * c + settings.format);
-    } else {
-      console.error("$('" + me.selector + "') doesn't exist!");
-      return me.selector;
-    }
-  };
-})(jQuery);
+      unit: 'px',
+    }, options);
 
-$(function() {
-  var scale;
-  scale = void 0;
+    // Remove last 2 characters from the string ( px or em etc.. )
+    l = me.css('font-size').length - 2;
+
+    // Clean size without unit
+    size = me.css('font-size').slice(0, l);
+
+    // Calc the final size. ( size by factor )
+    me.css('font-size', size * factor + settings.unit );
+
+    // Return the final size
+    return size * factor + settings.unit;
+  }
+} ( jQuery ) );
+
+
+$(document).ready(function() {
+  var url = 'http://github.com/Rawnly/Scale.js';
+  console.log('This website use Scale.js!');
+  console.log('Check it out! :' + url);
+
+
+  var scale, str, nSize;
+
   scale = function(el) {
-    var str;
-    str = void 0;
-    if ($(el).data('scale') && $(el).data('unit')) {
-      str = {
-        txt: $(el).css('font-size'),
-        size: $(el).css('font-size').length,
-        u: $(el).data('unit')
-      };
-      $.each($(el), function() {
-        var nSize;
-        nSize = void 0;
-        nSize = str.txt.slice(0, str.size - 2) * $(this).data('scale');
-        $(this).css('font-size', nSize + str.u);
-      });
-    } else if ($(el).data('scale') && !$(el).data('unit')) {
-      str = {
-        txt: $(el).css('font-size'),
-        size: $(el).css('font-size').length,
-        u: 'px'
-      };
-      console.warn('SCALE.JS: No elements with `data-unit` attribute. I will use default unit: ' + str.u);
-      $.each($(el), function() {
-        var nSize;
-        nSize = void 0;
-        nSize = str.txt.slice(0, str.size - 2) * $(this).data('scale');
-        $(this).css('font-size', nSize + str.u);
-      });
-    } else if (!$(el).data('scale') && !$(el).data('unit')) {
-      console.warn('SCALE.JS: No elements with `data-scale` & `data-unit` attribute.');
-    }
-  };
-  scale('[data-scale]');
 
-  /*
-  #ORIGINAL
-  $ ->
-    scale = (el) ->
-      if $(el).data('scale')
-        str =
-          txt: $(el).css('font-size')
-          size: $(el).css('font-size').length
-        $.each $(el), ->
-          nSize = str.txt.slice(0, str.size - 2) * $(this).data('scale')
-          $(this).css 'font-size', nSize
-          return
-      else
-        console.warn ' WARNING! No elements with `data-scale` attribute.'
-      return
-  
-    scale '[data-scale]'
-    return
-   */
+    if ( $(el).data('scale') && $(el).data('unit') ) {
+
+      // console.log(' ');
+      // console.log('Someone has datas');
+      // console.log(' ');
+
+      // Get datas from HTML and CSS
+      var str = {
+        fSize : $(el).css('font-size'),
+        leng : $(el).css('font-size').length,
+        unit : $(el).data('unit'),
+        factor : $(el).data('scale')
+      };
+
+      // Apply configuration to each element who has `data-scale` attr
+      $.each( $(el), function () {
+        $(this).css('font-size', str.fSize.slice( 0, str.leng - 2 ) * str.factor + str.unit )
+      } );
+
+    } else if ( !$(el).data('unit') && $(el).data('scale') ) {
+
+      var str = {
+        fSize : $(el).css('font-size'),
+        leng : $(el).css('font-size').length,
+        unit : 'px',
+        factor : $(el).data('scale')
+      };
+
+      // Apply configuration to each element who has `data-scale` attr
+      $.each( $(el), function () {
+        $(this).css('font-size', str.fSize.slice( 0, str.leng - 2 ) * str.factor + str.unit )
+      } );
+
+      console.warn('No unit defined! Default is `px`. (Scale.js)');
+
+
+    } else if ( !$(el).data('unit') && !$(el).data('scale') ) {
+
+      console.warn('No data-scale and data-unit founded! (Scale.js)');
+
+    } else {
+      console.log(' ');
+      console.error('OPS! An error is occurred (Scale.js)');
+      console.log(' ');
+    }
+
+  };
+
+  scale( $('[data-scale]') );
 });
