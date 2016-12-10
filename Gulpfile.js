d210450637112c5ec6f5e929f9d2bfd5d92dba0c
Rawnly <fedevitale99@gmail.com>
2016-12-10 03:14:40 +0100

    updated

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,66 +1,64 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
-var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
 var gutil = require('gulp-util');
-var notify = require('gulp-notify');
 var chalk = require('chalk');
+var connect = require('gulp-connect-php');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function () {
+
+  // File watching
   console.log(chalk.green.inverse(' Watching ') + chalk.inverse.bold(' for ') + chalk.inverse.red('changes...'));
 
+  // Watch files and call the task
   gulp.watch('./coffee/**/*.coffee', ['coffee']);
   gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./dist/**/**', ['reload']);
 
+  // More Specific
+  // gulp.watch('./dist/**/*.html', ['reload']);
+  // gulp.watch('./dist/js/**/*.js', ['reload']);
 
-  console.log(chalk.blue.inverse(' Running ') + chalk.bold.blue.inverse(' LOCAL ') + chalk.blue.inverse(' Server '));
-  gulp.src('./dist/')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: {
-          enable:true,
-          path: 'app'
-      },
-      open: true,
-      fallback: 'index.html',
-    }));
+  // BrowserSync init :)
+  browserSync.init({
+      server: {
+          baseDir: "./dist/example/",
+          proxy: 'mylocal.dev'
+      }
+  });
 });
 
+// Convert CoffeeScript to JavaScript
 gulp.task('coffee', function() {
   gulp.src('./coffee/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./dist/js/'));
+    browserSync.reload()
     console.log( chalk.yellow.inverse.bold('Coffescript ') + chalk.inverse('updated') );
 });
 
+// Convert SASS to CSS
 gulp.task('sass', function () {
-  console.log( chalk.yellow.inverse.bold('Sass ') + chalk.inverse('updated') );
   gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
+    browserSync.reload()
+    console.log( chalk.yellow.inverse.bold('Sass ') + chalk.inverse('updated') );
 });
 
-gulp.task('localhost', function () {
-  gulp.src('./dist/')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: {
-          enable:true,
-          path: 'app'
-      },
-      open: true,
-      fallback: 'index.html',
-    }));
+// Reload the webpage
+gulp.task('reload', function() {
+  browserSync.reload();
 });
 
-
-gulp.task('help', function () {
-  console.log('');
-  console.log('gulp' + ' ' + chalk.green('webserver') + '                 ' + chalk.gray('# Start a server.'));
-  console.log('gulp' + ' ' + chalk.green('watch') + '                     ' + chalk.gray('# Watch files.'));
-  console.log('');
+// If you want create a php local server
+gulp.task('phpserver', function() {
+  connect.server({
+    base: './dist'
+  }, function () {
+    browserSync({
+      proxy: '0.0.0.0:8000'
+    });
+  });
 });
-
-function webserver() {
-  console.log(chalk.green.inverse(' Running L O C A L Server '));
-}
